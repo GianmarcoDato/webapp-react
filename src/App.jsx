d6058230a225ea8df2d8d70ca34a5cc27178331f
GianmarcoDato <gianmarco.dato26@gmail.com>
2026-04-02 17:47:24 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div class='page'></div>
+
+
+      <h1>Benvenuto in movies.js</h1>
+    </>
+  )
+}
+
+export default App
